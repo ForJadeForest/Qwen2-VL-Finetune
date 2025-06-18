b48121f55c615b5c3d539e18b8b6d0603d8db992
yingzhepeng <yingzhepeng@tencent.com>
2025-06-18 23:18:11 +0800

    init

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -3,22 +3,23 @@
 # You can use 2B instead of 7B
 # MODEL_NAME="Qwen/Qwen2-VL-7B-Instruct"
 # MODEL_NAME="Qwen/Qwen2-VL-2B-Instruct"
-MODEL_NAME="Qwen/Qwen2.5-VL-3B-Instruct"
-# MODEL_NAME="Qwen/Qwen2.5-VL-7B-Instruct"
+# MODEL_NAME="Qwen/Qwen2.5-VL-3B-Instruct"
+MODEL_NAME="Qwen/Qwen2.5-VL-7B-Instruct"
 
-GLOBAL_BATCH_SIZE=128
-BATCH_PER_DEVICE=4
+GLOBAL_BATCH_SIZE=64
+
+BATCH_PER_DEVICE=1
 NUM_DEVICES=8
 GRAD_ACCUM_STEPS=$((GLOBAL_BATCH_SIZE / (BATCH_PER_DEVICE * NUM_DEVICES)))
 
 export PYTHONPATH=src:$PYTHONPATH
 
 deepspeed src/train/train_sft.py \
-    --use_liger True \
-    --deepspeed scripts/zero3_offload.json \
+    --use_liger False \
+    --deepspeed scripts/zero2.json \
     --model_id $MODEL_NAME \
-    --data_path /path/to/your/training/data.json \
-    --image_folder /path/to/your/image/folder \
+    --data_path /mnt/private_yingzhepeng/code/other/DeQA-Score/data/DIQA/train_diqa_color_fidelity.json \
+    --image_folder /mnt/private_yingzhepeng/code/other/DeQA-Score/data/train/res \
     --remove_unused_columns False \
     --freeze_vision_tower False \
     --freeze_llm False \
@@ -31,7 +32,7 @@ deepspeed src/train/train_sft.py \
     --per_device_train_batch_size $BATCH_PER_DEVICE \
     --gradient_accumulation_steps $GRAD_ACCUM_STEPS \
     --image_min_pixels $((512 * 28 * 28)) \
-    --image_max_pixels $((1280 * 28 * 28)) \
+    --image_max_pixels $((10000 * 28 * 28)) \
     --learning_rate 1e-5 \
     --merger_lr 1e-5 \
     --vision_lr 2e-6 \
@@ -46,4 +47,7 @@ deepspeed src/train/train_sft.py \
     --save_strategy "steps" \
     --save_steps 200 \
     --save_total_limit 10 \
-    --dataloader_num_workers 4
+    --dataloader_num_workers 4 \
+    --level_prefix "The quality of the image is" \
+    --level_names excellent good fair poor bad \
+    --softkl_loss True
