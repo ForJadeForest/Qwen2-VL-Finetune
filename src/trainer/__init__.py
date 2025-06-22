9d5506ef35fb8964bd4ae90ed0ece69a235bfa60
yingzhepeng <yingzhepeng@tencent.com>
2025-06-22 19:11:44 +0800

    delete

--- src/trainer/__init__.py
+++ src/trainer/__init__.py
@@ -1,5 +1,4 @@
-from .dpo_trainer import QwenDPOTrainer
 from .sft_trainer import QwenSFTTrainer
-# from .grpo_trainer import QwenGRPOTrainer
 
-__all__ = ["QwenSFTTrainer", "QwenDPOTrainer"]
+
+__all__ = ["QwenSFTTrainer"]
