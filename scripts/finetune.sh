d120326f00bf6d00f2434f54926bb3b0a0451e62
yingzhepeng <yingzhepeng@tencent.com>
2025-06-22 23:43:49 +0800

    update

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -6,7 +6,8 @@
 # MODEL_NAME="Qwen/Qwen2.5-VL-3B-Instruct"
 MODEL_NAME="Qwen/Qwen2.5-VL-7B-Instruct"
 
-GLOBAL_BATCH_SIZE=8
+GLOBAL_BATCH_SIZE=64
+export NCCL_DEBUG=WARN
 
 BATCH_PER_DEVICE=1
 NUM_DEVICES=8
@@ -18,24 +19,23 @@ deepspeed src/train/train_sft.py \
     --use_liger False \
     --deepspeed scripts/zero2.json \
     --model_id $MODEL_NAME \
-    --data_path /mnt/private_yingzhepeng/code/other/DeQA-Score/data/DIQA/train_diqa_color_fidelity.json \
-    --image_folder /mnt/private_yingzhepeng/code/other/DeQA-Score/data/train/res \
+    --data_path ./data/train_file/split/overall_quality/train.jsonl \
+    --val_path ./data/train_file/split/overall_quality/val.jsonl \
+    --image_folder ./data/DIQA/train/res \
     --remove_unused_columns False \
-    --freeze_vision_tower False \
+    --freeze_vision_tower True \
     --freeze_llm False \
-    --freeze_merger False \
+    --freeze_merger True \
     --bf16 True \
     --fp16 False \
     --disable_flash_attn2 False \
-    --output_dir output/test_fft \
-    --num_train_epochs 1 \
+    --output_dir output/deqa \
+    --num_train_epochs 2 \
     --per_device_train_batch_size $BATCH_PER_DEVICE \
     --gradient_accumulation_steps $GRAD_ACCUM_STEPS \
-    --image_min_pixels $((512 * 28 * 28)) \
-    --image_max_pixels $((10000 * 28 * 28)) \
+    --image_min_pixels $((4 * 28 * 28)) \
+    --image_max_pixels $((16000 * 28 * 28)) \
     --learning_rate 1e-5 \
-    --merger_lr 1e-5 \
-    --vision_lr 2e-6 \
     --weight_decay 0.1 \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
@@ -43,14 +43,13 @@ deepspeed src/train/train_sft.py \
     --tf32 True \
     --gradient_checkpointing True \
     --report_to tensorboard \
-    --lazy_preprocess True \
     --save_strategy "steps" \
-    --save_steps 200 \
-    --save_total_limit 10 \
+    --save_steps 100 \
+    --save_total_limit 2 \
     --dataloader_num_workers 16 \
     --level_prefix "The quality of the image is" \
     --level_names five four three two one \
     --softkl_loss True \
-    --train_deqa
-
+    --train_deqa \
+    --max_grad_norm 1
 
